beb78146826d71bb04e96501f7460e299090a85d
Xucz <823322977@qq.com>
2022-03-17 20:30:04 +0800

    First Version

--- spider_beike.py
+++ spider_beike.py
@@ -22,6 +22,7 @@ def get_bc(page = ''):
     }
 
     url_title = 'https://xa.zu.ke.com/'
+    # 如果需要查询别的地区的房子，直接改这个就行
     url = 'https://xa.zu.ke.com/ditiezufang/li38000009046408s38000009046433/'+page
 
     response = requests.get(url, headers=headers)
